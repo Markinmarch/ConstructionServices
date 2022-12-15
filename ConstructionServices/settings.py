9f289436f1a310c970d47c8c0c73208de3bb572a
Mark Izotov <iztvmrk@mail.ru>
2022-12-15 21:15:16 +0300

    add_database

--- ConstructionServices/settings.py
+++ ConstructionServices/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import configparser
+
+config = configparser.ConfigParser()
+config.read('passwords.ini')
+PASSWORD = config['PASSWORD_DB']['PASSWORD']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +80,13 @@ WSGI_APPLICATION = 'ConstructionServices.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ConstructionServices',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': PASSWORD,
+
     }
 }
 
