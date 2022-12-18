16a35271d6bac7a121d40c9d32a022356e861bbc
Mark Izotov <iztvmrk@mail.ru>
2022-12-18 21:06:19 +0300

    уточнение и редактирование моделей

--- Services/models.py
+++ Services/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 
 # Create your models here.
+# Модель, в которой указывается вид услуг, т.е. например: ремонт, демонтаж, проектирование, строительство, сантехнические работы и пр.
 class Sevices(models.Model):
     name_sevice = models.CharField(primary_key=True, max_length=50, verbose_name='Вид услуг')
     
     def __str__(self):
         return self.name_sevice
 
+# Модель, в котору указывается конкретный вид работ с ценой, например: кладка камня, установка унитаза или смесителей, поклейка обоев и пр.
 class Works(models.Model):
     name_work = models.ForeignKey(Sevices, on_delete=models.CASCADE, verbose_name='Наименование работ')
     price = models.CharField(max_length=50, null = True, verbose_name='Цена работ')
-    
+        
     def __str__(self):
         return self.name_work + '-->' + self.price
