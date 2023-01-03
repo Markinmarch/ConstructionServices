02088a1d379245f9d884f24163432a202fa68eee
Mark Izotov <iztvmrk@mail.ru>
2023-01-03 18:48:04 +0300

    edit_models

--- Services/admin.py
+++ Services/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
-
-from Services.models import Services, User_Client
+from .models import Order, Orders, Services, User_Client
 
 # Register your models here.
+
+class OrdersInline(admin.TabularInline):
+    model = Orders
+    extra = 0
+
 @admin.register(Services)
 class ServicesAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['name_service', 'price']
 
 @admin.register(User_Client)
 class User_ClientAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['name_client', 'phone', 'email']
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrdersInline,]
--- Services/models.py
+++ Services/models.py
@@ -1,22 +1,33 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
-# Модель, в которой указывается вид услуг, т.е. например: ремонт, демонтаж, проектирование, строительство, сантехнические работы и пр.
 class Services(models.Model):
-    name_sevice = models.CharField(primary_key=True, max_length=50, verbose_name='Наименование услуги')
+    name_service = models.CharField(max_length=50, verbose_name='Наименование услуги')
     price = models.IntegerField(verbose_name="Цена работ")
 
     def __str__(self):
-        return self.name_sevice
+        return self.name_service
 
 class User_Client(models.Model):
-    name_client = models.CharField(max_length=50, verbose_name='Имя клиента')
-    second_name_client = models.CharField(max_length=50, verbose_name='Фамилия клиента')
-    phone = models.CharField(max_length=12, blank=False, null = False, verbose_name='Телефон клиента')
+    name_client = models.CharField(primary_key=True, max_length=50, verbose_name='ФИО клиента', help_text='Например: Иванов Пётр Сергеевич')
+    phone = models.CharField(max_length=12, verbose_name='Телефон клиента')
     email = models.EmailField(max_length=254, verbose_name='Электронная почта')
 
     def __str__(self):
-        return f'{self.name_client} {self.second_name_client}'
+        return f'{self.name_client}'
 
-# class Orders(models.Model):
-    
+class Order(models.Model):
+    pass
+    # def __str__(self):
+    #     num_order = datetime.now().strftime('%d%m%Y%S')
+    #     return f'Заказ №{self.id}/{num_order}'
+
+class Orders(models.Model):
+    service = models.ForeignKey(Services, on_delete=models.CASCADE, related_name='positions', null = True)
+    name_client = models.ForeignKey(User_Client, related_name='positions', on_delete=models.CASCADE, null=True)
+    order = models.ForeignKey(Order, related_name='positions', on_delete=models.CASCADE, null=True, verbose_name='Заказ')
+    quantity = models.IntegerField(null=True)
+
+    def __str__(self):
+        return f'{self.name_client}'
