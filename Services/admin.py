7f5e0e8422ceb1af3218a1fdf029c000b900bb5a
Mark Izotov <iztvmrk@mail.ru>
2023-01-03 19:32:03 +0300

    drop_db

--- Services/admin.py
+++ Services/admin.py
@@ -1,20 +1,4 @@
 from django.contrib import admin
-from .models import Order, Orders, Services, User_Client
+from .models import 
 
 # Register your models here.
-
-class OrdersInline(admin.TabularInline):
-    model = Orders
-    extra = 0
-
-@admin.register(Services)
-class ServicesAdmin(admin.ModelAdmin):
-    list_display = ['name_service', 'price']
-
-@admin.register(User_Client)
-class User_ClientAdmin(admin.ModelAdmin):
-    list_display = ['name_client', 'phone', 'email']
-
-@admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):
-    inlines = [OrdersInline,]
