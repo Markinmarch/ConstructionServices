bba3db3dafee5c45e30d5ecc3087305b9138db51
Mark Izotov <iztvmrk@mail.ru>
2023-01-04 23:57:56 +0300

    finish_today

--- Services/models.py
+++ Services/models.py
@@ -13,25 +13,25 @@ class Client(models.Model):
         return f'{self.name} {self.second_name}'
 
 class Service(models.Model):
-    service = models.CharField(max_length = 20, verbose_name = 'Наименование услуги')
+    service = models.CharField(max_length = 20, verbose_name = 'Наименование услуги', null = True)
 
     def __str__(self):
         return self.service
 
 class Work(models.Model):
-    service = models.ForeignKey(Service, on_delete=models.CASCADE, related_name='work', verbose_name='Наименование услуги')
+    service = models.ForeignKey(Service, on_delete=models.CASCADE, related_name='work', verbose_name='Наименование услуги', null = True)
     work = models.CharField(max_length = 50, verbose_name = 'Наименование работ')
     price = models.DecimalField(max_digits=8, decimal_places=2, null = True, verbose_name = 'Цена работ')
     
     def __str__(self):
-        return f'{self.work} ₽'
+        return f'{self.work} ₽{self.price}'
 
 class ForInlineOrder(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='client', null = True)
     total_price = Work.objects.aggregate(Sum('price'))['price__sum']
 
     def __str__(self):
-        return f'{self.id} - {self.client}'
+        return f'{self.id} - {self.client} - {self.total_price}'
 
 class Order(models.Model):
     order = models.ForeignKey(Work, on_delete=models.CASCADE, related_name='order', null = True)
