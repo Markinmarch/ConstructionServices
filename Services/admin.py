9bd5ae8aaaec79178c17ff9adc5c5881dead06bd
Mark Izotov <iztvmrk@mail.ru>
2023-01-08 00:38:20 +0300

    ready_models

--- Services/admin.py
+++ Services/admin.py
@@ -1,3 +1,21 @@
 # from django.contrib import admin
-# from .models import Client, Order, Work
+from .models import Client, ListClients, Order, Item
 from django.contrib import admin
+
+@admin.register(ListClients)
+class ListClientsAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'second_name', 'phone', 'email']
+
+class InlineClient(admin.StackedInline):
+    model = Client  
+    extra = 1
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [InlineClient,]
+    list_display = ['order', 'client', 'total_price']
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['item_name', 'price']
--- Services/models.py
+++ Services/models.py
@@ -1,2 +1,53 @@
 from django.db import models
 from datetime import datetime
+
+# таблица создания клиентов
+class ListClients(models.Model):
+
+    first_name = models.CharField(max_length=20, verbose_name='Имя')
+    second_name = models.CharField(max_length=20, verbose_name='Фамилия')
+    phone = models.CharField(max_length=12, verbose_name='Номер телефона')
+    email = models.EmailField(max_length=254, verbose_name='Электронная почта')    
+
+    def __str__(self):
+        return f'{self.first_name} {self.second_name}'
+
+# таблица заказов
+class Order(models.Model):
+
+    def generate_num_order():
+        num_order = datetime.now().strftime('%S%d%m%Y%M')
+        return f'{num_order}'
+
+    order = models.CharField(default=generate_num_order(), max_length=50, verbose_name='Номер заказа')
+    client = models.ForeignKey(ListClients, on_delete=models.CASCADE, related_name='order')
+
+    def total_price(self):
+        return sum([
+            amount.total() for amount in Client.objects.filter(client=self)
+        ])
+
+    def __str__(self):
+        return f'{self.client} {self.total_price()}'
+
+# таблица продукта/вещи
+class Item(models.Model):
+
+    item_name = models.CharField(max_length=50, verbose_name='Название продукта')
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Цена')
+
+    def __str__(self):
+        return f'{self.item_name}'
+
+# таблица связывающая
+class Client(models.Model):
+
+    item_name = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='client')
+    client = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='var_client')
+    count = models.IntegerField(default=1, verbose_name='Количество')
+
+    def total(self):
+        return self.item_name.price * self.count
+
+    def __str__(self):
+        return f'{self.total()}'
