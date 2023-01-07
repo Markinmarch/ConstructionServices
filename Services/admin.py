6972f181b37dccb6afeb4edd70bd8df5859af210
Mark Izotov <iztvmrk@mail.ru>
2023-01-07 03:35:26 +0300

    всё с чистого листа

--- Services/admin.py
+++ Services/admin.py
@@ -1,20 +1,3 @@
 # from django.contrib import admin
-from .models import Client, Order, Work
+# from .models import Client, Order, Work
 from django.contrib import admin
-
-class WorkInline(admin.StackedInline):
-    model = Work
-    extra = 1
-
-@admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):
-    inlines = [WorkInline,]
-
-@admin.register(Client)
-class ClientAdmin(admin.ModelAdmin):
-    # inlines = [OrderInline,]
-    pass
-# @admin.register(Work)
-# class WorkAdmin(admin.ModelAdmin):
-    # inlines = [OrderInline,]
-    # pass
